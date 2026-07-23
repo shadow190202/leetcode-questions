b8e33d1bdff8b3e8bbc530ab762032d3ca1487eb
Shreyansh sahu <156871729+shadow190202@users.noreply.github.com>
2026-07-23 19:40:32 +0530

    Time: 121 ms (40.26%), Space: 19.8 MB (99.25%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -5,15 +5,12 @@ class Solution(object):
         :rtype: int
         """
         i, j = 0, len(height) - 1
-        arr = []
-        lower_side = 0
+        water = 0
         for _ in range(len(height)):
             lower_side = min(height[i],height[j])
-            area = lower_side*(j-i)
+            water = max(water, lower_side*(j-i))
             if height[i] < height[j]:
                 i += 1
-                arr.append(area)
             else:
                 j -= 1
-                arr.append(area)
-        return max(arr)
+        return water
