10fe249ad6e56090654388233986f066b7fed298
Shreyansh sahu <156871729+shadow190202@users.noreply.github.com>
2026-07-28 16:39:25 +0530

    Time: 4 ms (8.23%), Space: 12.4 MB (17.9%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -5,14 +5,14 @@ class Solution(object):
         :rtype: bool
         """
         # Best case
-        # return n > 0 and n & (n-1) == 0
+        return n > 0 and n & (n-1) == 0
 
         # Recursion
 
-        if n < 1:
-            return False
-        elif n == 1:
-            return True
-        elif n > 1 and  n%2 != 0:
-            return False
-        return self.isPowerOfTwo(n//2)
+        # if n < 1:
+        #     return False
+        # elif n == 1:
+        #     return True
+        # elif n > 1 and  n%2 != 0:
+        #     return False
+        # return self.isPowerOfTwo(n//2)
