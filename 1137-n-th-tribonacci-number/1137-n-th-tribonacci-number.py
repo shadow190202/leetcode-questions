f9d538f8850b844ce83d8162648b8a2fd069e1cf
Shreyansh sahu <156871729+shadow190202@users.noreply.github.com>
2026-07-28 16:25:19 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.53%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -4,7 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # Best Approach
         a,b,c = 0,1,1
         for _ in range(n):
             a,b,c = b,c,a+b+c
         return a
+
+        #  Recursion
+        # if n < 2:
+        #     return n
+        # elif n == 2:
+        #     return 1
+        # return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
